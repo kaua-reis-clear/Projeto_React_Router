2d75f98ddecc0a8b917b478fedd949b8c2f94132
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-14 09:36:58 -0300

    added a not found page in case of accessing an unexisting url.

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -13,6 +13,7 @@ const Content = (props) => (
       <Route path="/about" element={<About />} />
       <Route path="/param/:id" element={<Param />} />
       <Route path="/" exact element={<Home />} />
+      <Route path="*" exact element={<NotFound />} />
     </Routes>
   </main>
 );
--- src/views/examples/About.jsx
+++ src/views/examples/About.jsx
@@ -4,7 +4,6 @@ const About = props => (
     <div className="About">
         <h1>Sobre</h1>
         <h2>O nosso sistema foi criado...</h2>
-        Componente About
     </div>
 )
 
