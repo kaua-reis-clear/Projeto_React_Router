03318fda875c0f42b4997624840443f24d589711
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-14 09:29:40 -0300

    added react router to the project, linked routes, and passing url parameters.

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,20 @@
-import './Content.css'
-import React from 'react';
+import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-const Content = props => (
-    <main className="Content">
-        <h1>Componente Content</h1>
-    </main>
-)
+import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
 
-export default Content
+const Content = (props) => (
+  <main className="Content">
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/param/:id" element={<Param />} />
+      <Route path="/" exact element={<Home />} />
+    </Routes>
+  </main>
+);
+
+export default Content;
